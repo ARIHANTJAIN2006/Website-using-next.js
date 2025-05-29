fe7ed456ddf66cf96cb2a969ad39c370d25af6bd
Dreamscape <dreamscape.dev>
2025-05-29 15:53:44 +0530

    äeofnoaf

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,27 +1,16 @@
 interface PageParams {
-  params: {
-    id: string;
-  };
+  params: Promise<{ id: string }>;
 }
 
-export default function UserProfile({ params }: PageParams) {
+export default async function UserProfile({ params }: PageParams) {
+  const resolvedParams = await params;
   return (
     <div className="flex flex-col items-center justify-center min-h-screen font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-2xl">Profile</h1>
       <hr />
       <p className="text-2xl">
-        Profile Page <span className="bg-orange-500">{params.id}</span>
+        Profile Page <span className="bg-orange-500">{resolvedParams.id}</span>
       </p>
     </div>
   );
 }
-
-
-
-
-
-
-
-
-
-
